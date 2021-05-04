e500f76e8cee8802ef893889613d52c73164443d
Lars Wikman <lars@underjord.io>
2021-05-04 16:16:13 +0200

    Fix Tailwind JIT, experimental

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -48,6 +48,17 @@ module.exports = (env, options) => {
     plugins: [
       new MiniCssExtractPlugin({ filename: "../css/app.css" }),
       new CopyWebpackPlugin([{ from: "static/", to: "../" }]),
-    ].concat(devMode ? [new HardSourceWebpackPlugin()] : []),
+    ].concat(devMode ? [
+      new HardSourceWebpackPlugin(),
+      new HardSourceWebpackPlugin.ExcludeModulePlugin([
+        {
+            // HardSource works with mini-css-extract-plugin but due to how
+            // mini-css emits assets, assets are not emitted on repeated builds with
+            // mini-css and hard-source together. Ignoring the mini-css loader
+            // modules, but not the other css loader modules, excludes the modules
+            // that mini-css needs rebuilt to output assets every time.
+            test: /mini-css-extract-plugin[\\/]dist[\\/]loader/,
+        },
+    ])] : []),
   };
 };
