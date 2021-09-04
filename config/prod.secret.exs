681c6fe4c470a0edb330779d949810e4043811d8
Mohd Maqbool Alam <maqbool@maqbool.net>
2021-09-04 10:19:54 +0530

    update config to raise

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -5,7 +5,7 @@
 use Mix.Config
 
 database_url =
-  System.get_env("DATABASE_URL") ||"database.db" ||
+  System.get_env("DATABASE_URL")
     raise """
     environment variable DATABASE_URL is missing.
     For example: ecto://USER:PASS@HOST/DATABASE
